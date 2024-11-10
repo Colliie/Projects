4b885f538c6e0e50881ccc4baf1c38e85b6fd521
CollieGGs <149844660+CollieGGs@users.noreply.github.com>
2024-11-10 10:21:33 -0600

    Update files and fix bugs

--- student-management/src/main/java/com/example/student_management/Repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.student_management.Repository;
-
-import com.example.student_management.models.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-}
